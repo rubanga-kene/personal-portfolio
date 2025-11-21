9b3d6d22570eb5047d0c3fd5db3a62023c7176a3
Solomon <rubangakene20@gmail.com>
2025-11-21 16:09:24 +0300

    Add web services animation

--- components/WebsitesSection.tsx
+++ components/WebsitesSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Image from "next/image";
+// import Image from "next/image";
 import { SectionHeading } from "./section-heading";
 import AnimatedCard from "../components/AnimatedCard";
 // import { Button } from "./ui/button";
